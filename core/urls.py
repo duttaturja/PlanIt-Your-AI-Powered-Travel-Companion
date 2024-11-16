cf0c983f0ce8ae12f9381fe7a7896683394900d5
Sayemahamed <sayemahamed183@gmail.com>
2024-11-16 23:32:19 +0600

    implemented the user profile page

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = "core"
 urlpatterns = [
     path("index/", views.index, name="index"),
+    path("profile/", views.profile, name="profile"),
 ]
