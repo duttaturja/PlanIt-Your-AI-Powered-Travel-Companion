5778b518875ccd3e3941dc30baaea0e724c95aa2
Sayemahamed <sayemahamed183@gmail.com>
2024-11-16 19:41:58 +0600

    structured view and urls

--- planit/urls.py
+++ planit/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('__reload__/', include('django_browser_reload.urls')),
+    path("",include('core.urls')),
     path('', views.index, name='home'),
 ]
 
